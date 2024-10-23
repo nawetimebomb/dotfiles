265d13877f5c2f5a89885110ba08340c4de0f994
Nahuel J. Sacchetti <me@nsacchetti.com>
2024-10-23 16:55:32 -0500

    feat: add i3status configuration, revamp i3 configuration

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ echo "[symlink] bin -> ~/bin" &&
     ln -s ${dir}/bin/ ~/
 echo "[symlink] i3 -> ~/.config/i3" &&
     ln -s ${dir}/i3/ ~/.config/
-echo "[symlink] polybar -> ~/.config/polybar" &&
-    ln -s ${dir}/polybar/ ~/.config/
+echo "[symlink] i3status -> ~/.config/i3status" &&
+    ln -s ${dir}/i3status/ ~/.config/
--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,6 +7,6 @@ polybar-msg cmd quit
 # killall -q polybar
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar.log
-polybar 2>&1 | tee -a /tmp/polybar1.log & disown
-echo "Polybar launched..."
+# echo "---" | tee -a /tmp/polybar.log
+# polybar 2>&1 | tee -a /tmp/polybar1.log & disown
+# echo "Polybar launched..."
